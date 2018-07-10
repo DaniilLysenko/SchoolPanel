48a5e44f30598768540cbee5f895036f029a5883
DaniilLysenko <drovk199995@gmail.com>
2018-07-10 11:32:26 +0300

    add login admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdminController extends Controller
 {
@@ -15,5 +18,22 @@ class AdminController extends Controller
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController'
         ]);
+    }
+
+    /**
+     * @Route("/login")
+     * @Method({"POST"})
+     */
+
+    public function login()
+    {
+    	if (isset($_POST['login']) && isset($_POST['password'])) {
+    		if ($_POST['login'] === 'admin' && $_POST['password'] === 'admin') {
+    			return new JsonResponse(["status" => 200]);
+    		} else {
+    			return new JsonResponse(["error" => "data is incorrect"]);
+    		}
+    	}
+    	return new JsonResponse(["error" => "data is missing"]);
     }
 }
